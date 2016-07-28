8946799a0b462320cc85c69cbd231538c500e44d
Chagall <famp@operamail.com>
2016-07-27 17:37:07 -0700

    Concertadas o show image das segundas cartas

--- Project League/Project League/ViewController.swift
+++ Project League/Project League/ViewController.swift
@@ -68,19 +68,14 @@ class ViewController: UIViewController {
                 firstCardImageView.image = UIImage(named: "amumu.jpg")
                 firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
-			case 5:
+			case 6:
                 firstCardName.text = card.name
                 firstCardImageView.image = UIImage(named: "anivia.jpg")
                 firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break	
-			case 6:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "annie.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
-                break
 			case 7:
                 firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "dr_mundo.jpg")
+                firstCardImageView.image = UIImage(named: "annie.jpg")
                 firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 8:
@@ -158,114 +153,109 @@ class ViewController: UIViewController {
     func changeSecondCardImage(card:Card) {
         switch card.id {
             case 1:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "aatrox.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "aatrox.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
             case 2:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "ahri.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "ahri.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
             case 3:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "akali.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "akali.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
             case 4:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "alistar.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "alistar.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
             case 5:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "amumu.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "amumu.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
-			case 5:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "anivia.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
-                break	
 			case 6:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "annie.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
-                break
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "anivia.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                break	
 			case 7:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "dr_mundo.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "annie.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 8:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "ashe.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "ashe.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 9:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "aurelion_sol.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "aurelion_sol.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 10:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "azir.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "azir.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 11:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "bard.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "bard.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 12:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "blitzcrank.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "blitzcrank.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 13:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "brand.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "brand.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 14:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "braum.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "braum.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 15:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "caitlyn.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "caitlyn.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 16:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "cassiopeia.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "cassiopeia.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 17:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "cho_gath.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "cho_gath.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 18:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "corki.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "corki.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 19:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "darius.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "darius.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
 			case 20:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "diana.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "diana.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
                 break
             default:
-                firstCardName.text = card.name
-                firstCardImageView.image = UIImage(named: "teemo.jpg")
-                firstCardFrameImageView.image = UIImage(named: "challenger_frame.png")
+                secondCardName.text = card.name
+                secondCardImageView.image = UIImage(named: "teemo.jpg")
+                secondCardFrameImageView.image = UIImage(named: "challenger_frame.png")
         }
     }
 
